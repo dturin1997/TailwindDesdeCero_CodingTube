4efede25bef56f050457e1b92d2c779fcd597f62
Dario Turin <dturin1997@gmail.com>
2024-01-22 00:12:39 -0600

    Como aplicar colores en TAILWIND y personalizarlos

--- TailwindDesdeCero/tailwind.config.js
+++ TailwindDesdeCero/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "azul-claro": "#243cff",
+        "azul-oscuro": "#0d1b3e",
+      },
+    },
   },
   plugins: [],
 };
