dfef27bf62dcab6ce9fb1cb4fcc9ea302a382fec
Dario Turin <dturin1997@gmail.com>
2024-01-29 22:38:00 -0600

    Como trabajar con ANCHO y ALTO en Tailwind CSS

--- TailwindDesdeCero/tailwind.config.js
+++ TailwindDesdeCero/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         "azul-claro": "#243cff",
         "azul-oscuro": "#0d1b3e",
       },
+      spacing: {
+        42: "170px",
+      },
+      width: {
+        43: "180px",
+      },
     },
   },
   plugins: [],
